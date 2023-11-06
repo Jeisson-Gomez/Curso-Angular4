fa6a1eb2b8f13d54f84d2d00941a3e98a3cb14ad
Jey Jey <jeypitbullz4@gmail.com>
2023-11-05 20:27:31 -0500

    Actualizacion del proyecto

---
+++ src/app/Trabajadores/Trabajadores.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'Trabajadores',
+    templateUrl: './trabajadores.component.html'
+})
+
+export class TrabajadoresComponent{
+    public Nombre_Componente = "Componente de Frutas";
+    public Listado_Fruta = 'Naranja, Mandarina, Piña, Arandanos'
+}
