380801c69ef6465aaff59ef7bc52d2034d1e65f5
Jey Jey <jeypitbullz4@gmail.com>
2023-11-08 17:30:34 -0500

    Nuevo Componente

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,5 +1,6 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 //importacinon de los componentes
 import { TrabajadoresComponent } from "./Trabajadores/Trabajadores.component";
 import { EmpleadoComponent } from './Empleado/Empleado.component';
