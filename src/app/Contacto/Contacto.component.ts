4d5b7bfde71b6b959fedcc74a29dffe9d31c4bc6
Jey Jey <jeypitbullz4@gmail.com>
2023-11-08 22:12:06 -0500

    Actualizacion del proyecto

--- src/app/Contacto/Contacto.component.ts
+++ src/app/Contacto/Contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Router, ActivatedRoute, Params } from "@angular/router";
 
 @Component({
     selector: 'Contacto',
@@ -7,4 +8,20 @@ import { Component } from "@angular/core";
 
 export class ContactoComponent{
     public titulo = 'Pagina de contacto de la web';
+    public Parametro: any;
+
+    constructor(
+        private _Route: ActivatedRoute,
+        private _Router: Router
+    ){}
+
+    ngOnInit(){}
+
+    redirigir(){
+        this._Router.navigate(['/Contacto', 'github.com/Jeisson-Gomez']);
+    }
+
+    redirigirdos(){
+        this._Router.navigate(['/Pagina-Principal'])
+    }
 }
