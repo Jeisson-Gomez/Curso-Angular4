fcfea535df7412867169202c02fcf5b4b8d23923
Jey Jey <jeypitbullz4@gmail.com>
2023-11-09 19:03:36 -0500

    Actualizacion del proyecto

--- src/app/Hijo/Hijo.Component.ts
+++ src/app/Hijo/Hijo.Component.ts
@@ -1,14 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
     selector: 'Componente-Hijo',
     template: `
     <p>Este es el {{ title }}</p>
+    <ul>
+        <li>{{ Propiedad_uno }}</li>
+        <li>{{ Propiedad_dos }}</li>
+    </ul>
+
+    <button>
+        Enviar datos al padre
+    </button>
     `
 })
 
 export class HijoComponent{
     public title: string;
+
+    @Input('Texto_1') Propiedad_uno: string | undefined;
+    @Input('Texto_2') Propiedad_dos: string | undefined;
+
+    @Output() Desde_el_hijo = new EventEmitter;
+
 
     constructor(){
         this.title = "Componente hijo"
